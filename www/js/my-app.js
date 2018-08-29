69c498d3356cb9f284ce2063238e57091dba6c8c
Gabe <gabe+3axis-innovative@users.noreply.github.com>
2018-08-29 12:49:54 -0400

    Backend UI fixes
    -Javascript not initalizing
    -Toolbar needs work

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -6,10 +6,7 @@ var myApp = new Framework7();
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
-});
+var mainView = myApp.addView('.view-main');
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
@@ -17,44 +14,19 @@ $$(document).on('deviceready', function() {
 });
 
 
-// Now we need to run the code that will be executed only for About page.
 
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-})
-$$(document).on('click', 'clubsClick', function (e) {
-  console.log('link clicked');
-});
 // Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
+$$(document).on('pageInit', function () {
+
+    console.log("jsworking");
     // Get page data from event data
     var page = e.detail.page;
 
     //Suit input event handling
 
 
-    $$('#clubsClick').on('tab-active', function () {
-        console.log("clubs");
-    });
-    $$('#spadesClick').on('tab-active', function () {
-        console.log("spades");
-    });
-    $$('#heartsClick').on('tab-active', function () {
-        myApp.alert("hearts");
-    });
-    $$('#diamondsClick').on('tab-active', function () {
-        myApp.alert("diamonds");
-    });
     if (page.name === 'about') {
         // Following code will be executed for page with data-page attribute equal to "about"
         myApp.alert('Here comes About page');
     }
-})
-
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
+});
